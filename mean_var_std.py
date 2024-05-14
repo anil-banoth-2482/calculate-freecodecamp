1766ea0dc88ae4a8e664285db7bc3663ea6f8a2c
anil-banoth-2482 <anilb03032004@gmail.com>
2024-05-14 17:58:20 +0530

    final solution which satisfied all the the test cases

--- mean_var_std.py
+++ mean_var_std.py
@@ -1,36 +1,43 @@
 import numpy as np
-import sys
 
-def calculate(my_list):
-     print(my_list[0])
-     if(len(my_list)!=9):
-         
-         return ValueError("List must contain nine numbers.")
+def calculate(list):
+  if len(list) != 9:
+    raise ValueError ("List must contain nine numbers.")
+  array = np.array(list).reshape((3, 3))
+  calculations = {
+    "mean": [
+      np.mean(array, axis = 0).tolist(),
+      np.mean(array, axis = 1).tolist(),
+      np.mean(array.tolist())
+    ],
+    "variance": [
+      np.var(array, axis = 0).tolist(),
+      np.var(array, axis = 1).tolist(),
+      np.var(array) .tolist()    
+    ],
+    "standard deviation": [
+      np.std(array, axis = 0).tolist(),
+      np.std(array, axis = 1).tolist(),
+      np.std(array).tolist()     
+    ],  
+    "max": [
+      np.max(array, axis = 0).tolist(),
+      np.max(array, axis = 1).tolist(),
+      np.max(array.tolist())
+    ],
+    "min": [
+      np.min(array, axis = 0).tolist(),
+      np.min(array, axis = 1).tolist(),
+      np.min(array.tolist())
+    ],
+    "sum": [
+      np.sum(array, axis = 0).tolist(),
+      np.sum(array, axis = 1).tolist(),
+      np.sum(array.tolist())
+    ]  
+  }
 
-     array=np.array(my_list)
-     array1=array.reshape(3,3)
-     print(array1)
-     calculation={}
-     
-     calculation['mean']=[list(np.mean(array1,axis=0)),list(np.mean(array1,axis=1)),float(np.mean(array1))]
-     calculation['variance']=[list(np.var(array1,axis=0)),list(np.var(array1,axis=1)),float(np.var(array1))]
-     calculation['standard deviation']=[np.std(array1,axis=0),np.std(array1,axis=1),np.std(array1)]
-     calculation['max']=[list(np.max(array1,axis=0)),list(np.max(array1,axis=1)),np.max(array1)]
-     calculation['min']=[list(np.min(array1,axis=0)),list(np.min(array1,axis=1)),np.min(array1)]
-     calculation['sum']=[list(np.sum(array1,axis=0)),list(np.sum(array1,axis=1)),np.sum(array1)]
-     
-     return calculation
+  return calculations
 
-    
-
-if __name__=="__main__":
-   
-   input_list=list(map(int,input("enter the nine numbers sepatated by spaces: ").split()))
-   
-   try:
-       
-    print(calculate(input_list))
-   except ValueError as e:
-     print(e)
 
        
