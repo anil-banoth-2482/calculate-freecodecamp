d12d5bcb9cfe5d20a9597b760ddf3a043234efb6
anil-banoth-2482 <anilb03032004@gmail.com>
2024-05-14 10:24:31 +0530

    add code upto printing numpy list in 3x3 form

--- mean_var_std.py
+++ mean_var_std.py
@@ -1,8 +1,22 @@
 import numpy as np
+import sys
 
 def calculate(list):
 
+     array=np.array(list)
+     array1=array.reshape(3,3)
+     print(array1)
+    
 
 
+    
 
-    return calculations
+if __name__=="__main__":
+   input_str=input("enter the nine numbers sepatated by spaces: ")
+   input_list=input_str.split()
+   print(input_list)
+   while(len(input_list)!=9):
+       print("List must contain nine numbers.")
+       input_str=input("enter the nine numbers sepatated by spaces: ")
+   calculate(input_list)
+       
